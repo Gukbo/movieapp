f0c43a085c790ad28142d7071cf7c0677f158a93
Gukbo <2onesound@naver.com>
2025-03-19 18:39:25 +0900

    끝

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,5 +1,38 @@
-function Detail() {
-  return <h1>detail</h1>;
-}
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
+function Detail() {
+  const { id } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState([]);
+
+  const getDetails = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setDetails(() => json.data.movie);
+    setLoading(false);
+    console.log(json.data.movie);
+  };
+  useEffect(() => {
+    getDetails();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <img src={details.medium_cover_image} alt={details.title} />
+          <h2>{details.title}</h2>
+          <p>개봉년도 : {details.year}</p>
+          <p> ⭐ {details.rating}</p>
+        </div>
+      )}
+    </div>
+  );
+}
 export default Detail;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Movie from "../components/movie";
+import Movie from "../components/Movie";
 
 function Home() {
   const [status, setStatus] = useState(true);
